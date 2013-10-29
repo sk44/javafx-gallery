c5e95464365649a4d1dcef45ef2e5f4310d8fa5d
sk44 <zombie.squad.69.cbt@gmail.com>
2013-10-30 02:41:36 +0900

    ディレクトリ移動時に選択状態を記憶しておくようにした

--- src/main/java/sk44/jfxgallery/controllers/MainWindowController.java
+++ src/main/java/sk44/jfxgallery/controllers/MainWindowController.java
@@ -4,14 +4,15 @@
  */
 package sk44.jfxgallery.controllers;
 
-import sk44.jfxgallery.controllers.ConfigureWindowController;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -52,248 +53,278 @@ import sk44.jfxgallery.models.PathModel;
  */
 public class MainWindowController implements Initializable {
 
-	private final ObservableList<PathModel> directories = FXCollections.observableArrayList();
-	private Path currentPath;
-	private LoadImageTask loadImageTask;
-	@FXML
-	private TextField currentPathTextField;
-	@FXML
-	private ScrollPane flowScrollPane;
-	@FXML
-	private TilePane thumbnails;
-	@FXML
-	private ListView<PathModel> directoryView;
-	@FXML
-	private AnchorPane rootPane;
+    private static final int HISTORY_SIZE = 16;
+    private final LinkedHashMap<String, PathModel> histriesMap = new LinkedHashMap<String, PathModel>(HISTORY_SIZE, 0.75f, false) {
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<String, PathModel> eldest) {
+            return size() > HISTORY_SIZE;
+        }
+    };
+    private final ObservableList<PathModel> directories = FXCollections.observableArrayList();
+    private Path currentPath;
+    private LoadImageTask loadImageTask;
+    @FXML
+    private TextField currentPathTextField;
+    @FXML
+    private ScrollPane flowScrollPane;
+    @FXML
+    private TilePane thumbnails;
+    @FXML
+    private ListView<PathModel> directoryView;
+    @FXML
+    private AnchorPane rootPane;
 
-	@FXML
-	protected void handleKeyPressedOnList(KeyEvent event) {
-		if (event.getCode() == KeyCode.ENTER) {
-			moveToSelectedDirectory();
-		}
-	}
+    @FXML
+    protected void handleKeyPressedOnList(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            moveToSelectedDirectory();
+        }
+    }
 
-	@FXML
-	protected void handleConfigureAction(ActionEvent event) {
-		FXMLLoader loader = new FXMLLoader(getClass()
-			.getResource("/views/configureWindow.fxml"));
-		try {
-			loader.load();
-		} catch (IOException ex) {
-			Logger.getLogger(MainWindowController.class.getName()).log(Level.SEVERE, null, ex);
-			return;
-		}
-		ConfigureWindowController window = loader.getController();
-		window.showOn(rootPane);
-	}
+    @FXML
+    protected void handleConfigureAction(ActionEvent event) {
+        FXMLLoader loader = new FXMLLoader(getClass()
+            .getResource("/views/configureWindow.fxml"));
+        try {
+            loader.load();
+        } catch (IOException ex) {
+            Logger.getLogger(MainWindowController.class.getName()).log(Level.SEVERE, null, ex);
+            return;
+        }
+        ConfigureWindowController window = loader.getController();
+        window.showOn(rootPane);
+    }
 
-	@FXML
-	protected void handleMoveToAction(ActionEvent event) {
-		Path moveTo = new File(currentPathTextField.getText()).toPath();
-		if (Files.exists(moveTo) == false) {
-			System.err.println("moveTo " + moveTo.toString() + " does not exist.");
-			return;
-		}
-		moveTo(moveTo);
-	}
+    @FXML
+    protected void handleMoveToAction(ActionEvent event) {
+        Path moveTo = new File(currentPathTextField.getText()).toPath();
+        if (Files.exists(moveTo) == false) {
+            System.err.println("moveTo " + moveTo.toString() + " does not exist.");
+            return;
+        }
+        moveTo(moveTo);
+    }
 
-	@FXML
-	protected void handleDirectoriesMouseClicked(MouseEvent event) {
-		if (event.getButton().equals(MouseButton.PRIMARY)) {
-			moveToSelectedDirectory();
-		}
-	}
+    @FXML
+    protected void handleDirectoriesMouseClicked(MouseEvent event) {
+        if (event.getButton().equals(MouseButton.PRIMARY)) {
+            moveToSelectedDirectory();
+        }
+    }
 
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		directoryView.setCellFactory(new Callback<ListView<PathModel>, ListCell<PathModel>>() {
-			@Override
-			public ListCell<PathModel> call(ListView<PathModel> p) {
-				return new ListCell<PathModel>() {
-					@Override
-					protected void updateItem(PathModel t, boolean empty) {
-						super.updateItem(t, empty);
-						if (empty == false) {
-							setText(t.getName());
-						}
-					}
-				};
-			}
-		});
-		currentPathTextField.prefWidthProperty().bind(rootPane.widthProperty().subtract(180));
-		// flowPane が回りこまなくなるのでバインドしとく
-		thumbnails.prefWidthProperty().bind(flowScrollPane.widthProperty());
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        directoryView.setCellFactory(new Callback<ListView<PathModel>, ListCell<PathModel>>() {
+            @Override
+            public ListCell<PathModel> call(ListView<PathModel> p) {
+                return new ListCell<PathModel>() {
+                    @Override
+                    protected void updateItem(PathModel t, boolean empty) {
+                        super.updateItem(t, empty);
+                        if (empty == false) {
+                            setText(t.getName());
+                        }
+                    }
+                };
+            }
+        });
+        currentPathTextField.prefWidthProperty().bind(rootPane.widthProperty().subtract(180));
+        // flowPane が回りこまなくなるのでバインドしとく
+        thumbnails.prefWidthProperty().bind(flowScrollPane.widthProperty());
 
-		moveTo(Config.load().getStartupPath().toPath());
-	}
+        moveTo(Config.load().getStartupPath().toPath());
+    }
 
-	private void moveToSelectedDirectory() {
-		final PathModel selectedItem = directoryView.getSelectionModel().getSelectedItem();
-		if (selectedItem == null) {
-			return;
-		}
-		moveTo(selectedItem.getPath());
-	}
+    private void addHistory(PathModel item) {
+        histriesMap.put(currentPath.toString(), item);
+    }
 
-	private void moveTo(Path path) {
-		currentPath = path.toAbsolutePath().normalize();
-		currentPathTextField.setText(currentPath.toString());
-		directories.clear();
+    private PathModel getHistory() {
+        if (histriesMap.containsKey(currentPath.toString()) == false) {
+            return null;
+        }
+        return histriesMap.get(currentPath.toString());
+    }
 
-		if (currentPath.getParent() != null) {
-			// 上へ
-			directories.add(PathModel.parentModelForPath(path.getParent()));
-		}
-		try (DirectoryStream<Path> stream = Files.newDirectoryStream(currentPath)) {
-			for (Path entry : stream) {
-				if (Files.isDirectory(entry)) {
-					directories.add(PathModel.modelForPath(entry));
-				}
-			}
-			// TODO sort
-			directoryView.setItems(directories);
-			loadImages();
-		} catch (IOException ex) {
-			throw new RuntimeException(ex);
-		}
-	}
+    private void moveToSelectedDirectory() {
+        final PathModel selectedItem = directoryView.getSelectionModel().getSelectedItem();
+        if (selectedItem == null) {
+            return;
+        }
+        addHistory(selectedItem);
+        moveTo(selectedItem.getPath());
+    }
 
-	private void loadImages() {
-		// 画像ロード中に移動する場合、いったんキャンセル
-		if (loadImageTask != null && loadImageTask.isDone() == false) {
-			System.out.println("task will cancel.");
-			loadImageTask.onCancelled = new Runnable() {
-				@Override
-				public void run() {
-					Platform.runLater(new Runnable() {
-						@Override
-						public void run() {
-							startLoadImages();
-						}
-					});
-				}
-			};
-			loadImageTask.cancel();
-			loadImageTask = null;
-			System.out.println("task cancel completed.");
-			return;
-		}
-		startLoadImages();
-	}
+    private void moveTo(Path path) {
+        currentPath = path.toAbsolutePath().normalize();
+        currentPathTextField.setText(currentPath.toString());
+        directories.clear();
 
-	private void startLoadImages() {
+        if (currentPath.getParent() != null) {
+            // 上へ
+            directories.add(PathModel.parentModelForPath(path.getParent()));
+        }
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(currentPath)) {
+            for (Path entry : stream) {
+                if (Files.isDirectory(entry)) {
+                    directories.add(PathModel.modelForPath(entry));
+                }
+            }
+            // TODO sort
+            directoryView.setItems(directories);
+            PathModel select = getHistory();
+            if (select != null) {
+                directoryView.getSelectionModel().select(select);
+                // runLater かまさないと一番下までスクロールしてしまう
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        directoryView.scrollTo(directoryView.getSelectionModel().getSelectedIndex());
+                    }
+                });
+            }
+            loadImages();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 
-		thumbnails.getChildren().clear();
-		// ファイルが大量にある場合遅いので別スレッドで読み込み.
-		loadImageTask = new LoadImageTask();
-		Thread t = new Thread(loadImageTask);
-		t.setDaemon(true);
-		t.start();
-	}
+    private void loadImages() {
+        // 画像ロード中に移動する場合、いったんキャンセル
+        if (loadImageTask != null && loadImageTask.isDone() == false) {
+            System.out.println("task will cancel.");
+            loadImageTask.onCancelled = new Runnable() {
+                @Override
+                public void run() {
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            startLoadImages();
+                        }
+                    });
+                }
+            };
+            loadImageTask.cancel();
+            loadImageTask = null;
+            System.out.println("task cancel completed.");
+            return;
+        }
+        startLoadImages();
+    }
 
-	class LoadImageTask extends Task<Void> {
+    private void startLoadImages() {
 
-		Runnable onCancelled;
+        thumbnails.getChildren().clear();
+        // ファイルが大量にある場合遅いので別スレッドで読み込み.
+        loadImageTask = new LoadImageTask();
+        Thread t = new Thread(loadImageTask);
+        t.setDaemon(true);
+        t.start();
+    }
 
-		@Override
-		protected Void call() throws Exception {
-			System.out.println("loading start.");
+    class LoadImageTask extends Task<Void> {
 
-			try (DirectoryStream<Path> stream = Files
-				.newDirectoryStream(currentPath, "*.{jpg,jpeg,png,gif}")) {
+        Runnable onCancelled;
 
-				int index = 0;
-				for (final Path entry : stream) {
-					if (isCancelled()) {
-						System.out.println("task cancelled.");
-						if (onCancelled != null) {
-							onCancelled.run();
-						}
-						break;
-					}
-					if (Files.isDirectory(entry)) {
-						continue;
-					}
-					final Image image = new Image(Files.newInputStream(entry), 120.0, 100.0, true, true);
-					final ImageView imageView = new ImageView(image);
-					imageView.setCache(true);
-					// TODO 高さが揃わない
-					imageView.setFitHeight(100.0);
-					imageView.setFitWidth(120.0);
-					imageView.setPreserveRatio(true);
-					final String fileName = entry.getFileName().toString();
+        @Override
+        protected Void call() throws Exception {
+            System.out.println("loading start.");
 
-					final Button button = new Button(fileName);
-					button.setPrefHeight(110.0);
-					button.setPrefWidth(130.0);
-					button.setGraphic(imageView);
-					button.setContentDisplay(ContentDisplay.TOP);
-					button.setTextAlignment(TextAlignment.JUSTIFY);
-					button.getStyleClass().add("thumbnail");
+            try (DirectoryStream<Path> stream = Files
+                .newDirectoryStream(currentPath, "*.{jpg,jpeg,png,gif}")) {
 
-					final int current = index;
-					button.setOnAction(new EventHandler<ActionEvent>() {
-						@Override
-						public void handle(ActionEvent t) {
-							Platform.runLater(new Runnable() {
-								@Override
-								public void run() {
-									openImage(
-										new ImageWindowArgs(
-											entry.toAbsolutePath(),
-											current,
-											new ImageWindowArgs.IndexToPathFunction() {
-												@Override
-												public Path pathOfIndex(int index) {
-													// TODO いろいろつらい
-													List<Node> labels = thumbnails.getChildren();
-													if (index < 0 || labels.size() - 1 < index) {
-														return null;
-													}
-													Button l = (Button) labels.get(index);
-													return new File(currentPath.toFile(),
-														l.getText()).toPath();
-												}
-											}));
-								}
-							});
-						}
-					});
+                int index = 0;
+                for (final Path entry : stream) {
+                    if (isCancelled()) {
+                        System.out.println("task cancelled.");
+                        if (onCancelled != null) {
+                            onCancelled.run();
+                        }
+                        break;
+                    }
+                    if (Files.isDirectory(entry)) {
+                        continue;
+                    }
+                    final Image image = new Image(Files.newInputStream(entry), 120.0, 100.0, true, true);
+                    final ImageView imageView = new ImageView(image);
+                    imageView.setCache(true);
+                    // TODO 高さが揃わない
+                    imageView.setFitHeight(100.0);
+                    imageView.setFitWidth(120.0);
+                    imageView.setPreserveRatio(true);
+                    final String fileName = entry.getFileName().toString();
 
-					Platform.runLater(
-						new Runnable() {
-							@Override
-							public void run() {
-								// new Tooltip はメインスレッドで動かす必要があるっぽい
-								// http://stackoverflow.com/questions/13864182/create-new-tooltip-on-not-javafx-application-thread
-								final Tooltip tooltip = new Tooltip(fileName);
-								tooltip.getStyleClass().add("filenameTooltip");
-								button.setTooltip(tooltip);
-								button.setGraphic(imageView);
-								thumbnails.getChildren().add(button);
-							}
-						});
-					index++;
-				}
-			} catch (IOException ex) {
-				Logger.getLogger(MainWindowController.class.getName()).log(Level.SEVERE, null, ex);
-			}
+                    final Button button = new Button(fileName);
+                    button.setPrefHeight(110.0);
+                    button.setPrefWidth(130.0);
+                    button.setGraphic(imageView);
+                    button.setContentDisplay(ContentDisplay.TOP);
+                    button.setTextAlignment(TextAlignment.JUSTIFY);
+                    button.getStyleClass().add("thumbnail");
 
-			return null;
-		}
-	}
+                    final int current = index;
+                    button.setOnAction(new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent t) {
+                            Platform.runLater(new Runnable() {
+                                @Override
+                                public void run() {
+                                    openImage(
+                                        new ImageWindowArgs(
+                                            entry.toAbsolutePath(),
+                                            current,
+                                            new ImageWindowArgs.IndexToPathFunction() {
+                                                @Override
+                                                public Path pathOfIndex(int index) {
+                                                    // TODO いろいろつらい
+                                                    List<Node> labels = thumbnails.getChildren();
+                                                    if (index < 0 || labels.size() - 1 < index) {
+                                                        return null;
+                                                    }
+                                                    Button l = (Button) labels.get(index);
+                                                    return new File(currentPath.toFile(),
+                                                        l.getText()).toPath();
+                                                }
+                                            }));
+                                }
+                            });
+                        }
+                    });
 
-	private void openImage(ImageWindowArgs args) {
-		Config config = Config.load();
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass()
-				.getResource(config.getViewerMode().fxmlPath()));
-			loader.load();
-			ViewerController imageWindow = loader.getController();
-			imageWindow.showOn(rootPane, args);
-		} catch (IOException ex) {
-			Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
-		}
-	}
+                    Platform.runLater(
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                // new Tooltip はメインスレッドで動かす必要があるっぽい
+                                // http://stackoverflow.com/questions/13864182/create-new-tooltip-on-not-javafx-application-thread
+                                final Tooltip tooltip = new Tooltip(fileName);
+                                tooltip.getStyleClass().add("filenameTooltip");
+                                button.setTooltip(tooltip);
+                                button.setGraphic(imageView);
+                                thumbnails.getChildren().add(button);
+                            }
+                        });
+                    index++;
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(MainWindowController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            return null;
+        }
+    }
+
+    private void openImage(ImageWindowArgs args) {
+        Config config = Config.load();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass()
+                .getResource(config.getViewerMode().fxmlPath()));
+            loader.load();
+            ViewerController imageWindow = loader.getController();
+            imageWindow.showOn(rootPane, args);
+        } catch (IOException ex) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
 }
--- src/main/java/sk44/jfxgallery/models/PathModel.java
+++ src/main/java/sk44/jfxgallery/models/PathModel.java
@@ -4,7 +4,12 @@
  */
 package sk44.jfxgallery.models;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -12,30 +17,67 @@ import java.nio.file.Path;
  */
 public class PathModel {
 
-	private final Path path;
-	private final boolean parent;
+    private final Path path;
+    private final boolean parent;
 
-	public static PathModel modelForPath(Path path) {
-		return new PathModel(path, false);
-	}
+    public static PathModel modelForPath(Path path) {
+        return new PathModel(path, false);
+    }
 
-	public static PathModel parentModelForPath(Path path) {
-		return new PathModel(path, true);
-	}
+    public static PathModel parentModelForPath(Path path) {
+        return new PathModel(path, true);
+    }
 
-	private PathModel(Path path, boolean parent) {
-		this.path = path.toAbsolutePath().normalize();
-		this.parent = parent;
-	}
+    private PathModel(Path path, boolean parent) {
+        this.path = path.toAbsolutePath().normalize();
+        this.parent = parent;
+    }
 
-	public Path getPath() {
-		return path;
-	}
+    public Path getPath() {
+        return path;
+    }
 
-	public String getName() {
-		if (parent) {
-			return "..";
-		}
-		return path.getFileName().toString();
-	}
+    public String getName() {
+        if (parent) {
+            return "..";
+        }
+        return path.getFileName().toString();
+    }
+
+    public boolean isSamePath(Path other) {
+        try {
+            return Files.isSameFile(path, other);
+        } catch (IOException ex) {
+            Logger.getLogger(PathModel.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    // selectionModel で同一性判定が必要っぽいので実装しておく
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + Objects.hashCode(this.path);
+        hash = 37 * hash + (this.parent ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final PathModel other = (PathModel) obj;
+        if (!Objects.equals(this.path, other.path)) {
+            return false;
+        }
+        if (this.parent != other.parent) {
+            return false;
+        }
+        return true;
+    }
+
 }
